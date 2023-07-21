3a6d6e0bc08dbfc977a6a281cdbfbee2e3d99f20
lotfi0310 <lotfilouiz@gmail.com>
2023-07-21 17:44:25 +0200

    correction extension image

--- src/main/java/com/pi/Centrale_Achat/CentraleAchatApplication.java
+++ src/main/java/com/pi/Centrale_Achat/CentraleAchatApplication.java
@@ -1,10 +1,12 @@
 package com.pi.Centrale_Achat;
 
 import  org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -19,6 +21,14 @@ public class CentraleAchatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CentraleAchatApplication.class, args);
 	}
+	
+	 public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins("http://localhost:4200") // Replace this with the origin of your Angular app
+	                .allowedMethods("GET", "POST", "PUT", "DELETE")
+	                .allowedHeaders("*");
+	    }
+	 
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
